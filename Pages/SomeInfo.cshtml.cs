210fbe0c140c736ee7d012d5994167c170d331fa
Youssef Nafaa <nafaayoussef@gmail.com>
2020-11-15 20:46:14 -0500

    Web App

---
+++ Pages/SomeInfo.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppYN.Pages
+{
+    public class SomeInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
